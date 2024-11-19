9250846081634748e14b3b97257f0af0bdec699d
Ramsey <someramseyy@gmail.com>
2024-11-19 20:57:04 +0100

    Impl copy_command and fix chain ownership

--- src/task/parse.rs
+++ src/task/parse.rs
@@ -28,16 +28,16 @@ pub struct ParseError {
     pub position: TokenPosition,
 }
 
-enum Modifier<'a> {
-    At(&'a str),
-    To(&'a str),
+enum Modifier {
+    At(Arc<str>),
+    To(Arc<str>),
 }
 
-impl Clone for Modifier<'_> {
+impl Clone for Modifier {
     fn clone(&self) -> Self {
         match self {
-            Modifier::At(str) => Modifier::At(str),
-            Modifier::To(str) => Modifier::To(str),
+            Modifier::At(arc) => Modifier::At(arc.clone()),
+            Modifier::To(arc) => Modifier::To(arc.clone()),
         }
     }
 }
@@ -81,6 +81,23 @@ impl ParseContext {
         self.failed = true;
     }
 
+    pub fn expect_symbol(&mut self, symbol: char) -> bool {
+        if let Some(Token { data, position }) = self.next() {
+            match data {
+                TokenData::Symbol(ch) if ch == symbol => return true,
+
+                _ => {
+                    let kind = data.kind();
+                    self.err(Arc::from(format!("Expected '{symbol}', found {kind}")), position)
+                }
+            }
+        } else {
+            self.err(Arc::from(format!("Expected '{symbol}'")), self.pos.clone());
+        }
+
+        return false;
+    }
+
     pub fn expect_segment(&mut self, segment: &str) -> bool {
         if let Some(Token { data, position }) = self.next() {
             match data {
@@ -121,7 +138,7 @@ pub fn parse(data: Vec<Token>) -> Result<Vec<Instruction>, Vec<ParseError>> {
     let mut context = ParseContext::new(iterator);
 
     while !context.done {
-        begin_command(&mut context, Vec::new());
+        parse_command(&mut context);
     }
 
     if context.failed {
@@ -131,30 +148,109 @@ pub fn parse(data: Vec<Token>) -> Result<Vec<Instruction>, Vec<ParseError>> {
     }
 }
 
-fn begin_command(context: &mut ParseContext, chain: Vec<Modifier>) -> () {
+fn begin_chain(context: &mut ParseContext, chain: Vec<Modifier>, str: &str, position: TokenPosition) {
+    match str {
+        "at" => at_modifier(context, chain),
+        "to" => to_modifier(context, chain),
+        "copy" => copy_command(context, chain),
+        _ => context.err(Arc::from(format!("Unknown command '{str}'")), position),
+    }
+}
+
+fn parse_command(context: &mut ParseContext) {
     if let Some(Token { data: TokenData::String(str), position }) = context.next() {
-        match str.as_ref() {
-            "at" => at_modifier(context, chain),
-            "to" => to_modifier(context, chain),
-            _ => context.err(Arc::from(format!("Unknown command '{str}'")), position),
-        }
+        begin_chain(context, Vec::new(), str.as_ref(), position);
     } else {
         context.err(Arc::from("Expected command"), context.pos.clone());
     }
 }
 
-fn at_modifier(context: &mut ParseContext, mut chain: Vec<Modifier>) {
-    if let Some(str) = context.expect_string() {
-        chain.push(Modifier::At(str.as_ref()));
+fn parse_scope(context: &mut ParseContext, chain: Vec<Modifier>) {
+    if !context.expect_symbol('{') {
+        return;
     }
 
-    begin_command(context, chain);
+    while !context.done {
+        if let Some(Token { data, position }) = context.next() {
+            match data {
+                TokenData::Symbol('}') => break,
+                TokenData::String(str) => begin_chain(context, chain.clone(), str.as_ref(), position),
+                _ => context.err(Arc::from("Expected command or '}'"), position),
+            }
+        } else {
+            context.err(Arc::from("Unclosed scope"), context.pos.clone());
+        }
+    }
+}
+
+fn at_modifier(context: &mut ParseContext, mut chain: Vec<Modifier>) {
+    if let Some(str) = context.expect_string() {
+        chain.push(Modifier::At(str));
+    }
+
+    parse_scope(context, chain);
 }
 
 fn to_modifier(context: &mut ParseContext, mut chain: Vec<Modifier>) {
     if let Some(str) = context.expect_string() {
-        chain.push(Modifier::To(str.as_ref()));
+        chain.push(Modifier::To(str));
     }
 
-    begin_command(context, chain);
+    parse_scope(context, chain);
+}
+
+fn copy_command(context: &mut ParseContext, chain: Vec<Modifier>) {
+    let mut origin: Vec<Arc<str>> = Vec::new();
+    let mut target: Vec<Arc<str>> = Vec::new();
+
+    for modifier in chain {
+        match modifier {
+            Modifier::At(str) => origin.push(str),
+            Modifier::To(str) => target.push(str),
+
+            _ => context.err(Arc::from("Invalid modifier for 'copy', expected 'at' or 'to'"), context.pos.clone()),
+        }
+    }
+
+    while let Some(Token { data, position }) = context.next() {
+        match data {
+            TokenData::Symbol(';') => break,
+            TokenData::Segment(str) => {
+                match str.as_ref() {
+                    "at" => {
+                        if !read_string(context, &mut origin) {
+                            break;
+                        }
+                    }
+                    "to" => {
+                        if !read_string(context, &mut target) {
+                            break;
+                        }
+                    }
+
+                    _ => context.err(Arc::from(format!("Unknown command '{str}'")), position),
+                }
+            }
+            _ => context.err(Arc::from("Expected command attribute"), position),
+        }
+    }
+
+    if origin.is_empty() {
+        context.err(Arc::from("Expected at least one origin path"), context.pos.clone());
+    }
+
+    if target.is_empty() {
+        context.err(Arc::from("Expected at least one target path"), context.pos.clone());
+    }
+
+    // context.instructions.push(Instruction::Copy { origin, target });
+}
+
+fn read_string(context: &mut ParseContext, vec: &mut Vec<Arc<str>>) -> bool {
+    if let Some(str) = context.expect_string() {
+        vec.push(str);
+        true
+    } else {
+        false
+    }
 }
